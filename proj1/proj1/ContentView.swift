98f24354248898ffa43bf24c7459c0d9c03eabae
bkletz18 <112676159+bkletz18@users.noreply.github.com>
2024-01-22 10:03:39 -0500

    added code

---
+++ proj1/proj1/ContentView.swift
@@ -0,0 +1,45 @@
+//
+//  ContentView.swift
+//  proj1
+//
+//  Created by Ben Kletzing on 1/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HStack{
+            CardView(isFaceUp: true)
+            CardView()
+            CardView()
+            CardView()
+        }
+        
+        .foregroundColor(.orange)
+        .padding()     }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool = false
+    
+    var body: some View {
+        ZStack(content: {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 12)
+                    .strokeBorder(lineWidth:2)
+                RoundedRectangle(cornerRadius: 12)
+                    .strokeBorder(lineWidth: 2)
+                Text("👻").font(.largeTitle)
+            } else {
+                RoundedRectangle(cornerRadius: 12)
+            }
+        })
+    }
+    
+    
+}
+
+#Preview {
+    ContentView()
+}
---
+++ proj1/proj1/proj1App.swift
@@ -0,0 +1,17 @@
+//
+//  proj1App.swift
+//  proj1
+//
+//  Created by Ben Kletzing on 1/18/24.
+//
+
+import SwiftUI
+
+@main
+struct proj1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
